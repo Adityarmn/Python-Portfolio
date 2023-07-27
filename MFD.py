534977d92fd4936ad75058edab35f1f15ea66be5
Adityarmn <adityarahman187@gmail.com>
2023-07-27 15:26:30 +0700

    update PSO 27/7/23

--- MFD.py
+++ MFD.py
@@ -2,62 +2,70 @@ import pandas as pd
 import numpy as np
 import xml.etree.ElementTree as ET
 
-def get_data_group(df_flow_and_density): #grouping data to pandas object
-    #get flow(veh/hr) and density(veh/km) in each lane for certain interval 
+
+def get_data_group(df_flow_and_density):  # grouping data to pandas object
+    # get flow(veh/hr) and density(veh/km) in each lane for certain interval
     flowdensity = df_flow_and_density
 
-    #only for get a list of 'time' in mfd-data.csv
-    flowdensity_lane = flowdensity.groupby('lane_ids')
-    dummy_group = flowdensity_lane.get_group('1_1')
-    time_list = dummy_group['time']
+    # only for get a list of 'time' in mfd-data.csv
+    flowdensity_lane = flowdensity.groupby("lane_ids")
+    dummy_group = flowdensity_lane.get_group("1_1")
+    time_list = dummy_group["time"]
 
-    #real grouping (group by time)
-    flowdensity_time = flowdensity.groupby('time')
+    # real grouping (group by time)
+    flowdensity_time = flowdensity.groupby("time")
     return time_list, flowdensity_time
 
-def get_MFD_property(time_list, flowdensity_time,total_lane_length,lane_length_dict): #calculate Qt and Kt
-    Q = [] #list of network flow
-    K = [] #list of network density
-    #calculate network flow (Qt) and network density(Kt)
+
+def get_MFD_property(
+    time_list, flowdensity_time, total_lane_length, lane_length_dict
+):  # calculate Qt and Kt
+    Q = []  # list of network flow
+    K = []  # list of network density
+    # calculate network flow (Qt) and network density(Kt)
     for time in time_list:
         the_group = flowdensity_time.get_group(time)
-        lane_ids = the_group['lane_ids']
-        lane_flows = the_group['outflow']
-        lane_densities = the_group['laneDensity']
+        lane_ids = the_group["lane_ids"]
+        lane_flows = the_group["outflow"]
+        lane_densities = the_group["laneDensity"]
         flowx = []
-        densityx= []
-        #bisa dibenerin tanpa enumerate 
-        for i,lane in enumerate(lane_ids):
+        densityx = []
+        # bisa dibenerin tanpa enumerate
+        for i, lane in enumerate(lane_ids):
             lane_id = lane_ids.iloc[i]
             lane_flow = lane_flows.iloc[i]
             lane_densities = lane_densities.astype(float)
             lane_density = lane_densities.iloc[i]
-            lane_length = lane_length_dict[str(lane_id)] #get lane length from earlier dictionary in lane-length.csv
-        
-            flowxlength = lane_flow*lane_length
-            flowx.append(flowxlength) #list
-            densityxlength = lane_density*lane_length
-            densityx.append(densityxlength) #list
-        Qt = sum(flowx)/total_lane_length
-        Kt = sum(densityx)/total_lane_length
+            lane_length = lane_length_dict[
+                str(lane_id)
+            ]  # get lane length from earlier dictionary in lane-length.csv
+
+            flowxlength = lane_flow * lane_length
+            flowx.append(flowxlength)  # list
+            densityxlength = lane_density * lane_length
+            densityx.append(densityxlength)  # list
+        Qt = sum(flowx) / total_lane_length
+        Kt = sum(densityx) / total_lane_length
         Q.append(Qt)
         K.append(Kt)
     return Q, K
 
-def get_lane_length(df_lanelength): #get lane length information
-    lanelength_data = pd.read_csv(df_lanelength, skiprows=1, header=None) 
+
+def get_lane_length(df_lanelength):  # get lane length information
+    lanelength_data = pd.read_csv(df_lanelength, skiprows=1, header=None)
     lane_list = lanelength_data[0]
-    length_list = lanelength_data[1]/1000
-    #store in dictionary : 
-    lane_length = dict(zip(lane_list,length_list))
+    length_list = lanelength_data[1] / 1000
+    # store in dictionary :
+    lane_length = dict(zip(lane_list, length_list))
     return lane_length
 
+
 def MFD():
-    #Bikin program buat ngitung Ppeak Xpeak Pgridlock Xgridlock
+    # Bikin program buat ngitung Ppeak Xpeak Pgridlock Xgridlock
     # 1. Load data XML pakai pandas read_xml dari folder net_jkt-new/output/lane-data
     # 2. Bikin MFD jadi data Kn Qn (flow banding density)
     # 3. Hitung Qpeak dll
-    
+
     # Define a list to store the data
     time_list = {}
     flowdensity_time = {}
@@ -65,58 +73,61 @@ def MFD():
     Kn = {}
 
     lane_length_dict = get_lane_length("lane-length-new.csv")
-    total_lane_length = sum(lane_length_dict.values()) #total lane length in network (KM)
+    total_lane_length = sum(
+        lane_length_dict.values()
+    )  # total lane length in network (KM)
 
     data = []
-    tree = ET.parse('net_jkt-new/output/lane-data.xml')
+    tree = ET.parse("net_jkt-new/output/lane-data.xml")
     root = tree.getroot()
 
     # Iterate over all "interval" elements in the root
     for interval in root:
         # Get the attributes of the interval
         interval_attribs = interval.attrib
-        
+
         # Iterate over all "edge" elements in the interval
         for edge in interval:
             # Get the attributes of the edge
             edge_attribs = edge.attrib
-            
+
             # Iterate over all "lane" elements in the edge
             for lane in edge:
                 # Get the attributes of the lane
                 lane_attribs = lane.attrib
-                
+
                 # Combine the attributes of the interval, edge, and lane
                 combined_attribs = {**interval_attribs, **edge_attribs, **lane_attribs}
-                
+
                 # Append the combined attributes to the data list
                 data.append(combined_attribs)
 
     # Convert the data list to a DataFrame
     df = pd.DataFrame(data)
 
-    df['departed'] = df['departed'].astype(int)
-    df['entered'] = df['entered'].astype(int)
-    df['arrived'] = df['arrived'].astype(int)
-    df['left'] = df['left'].astype(int)
+    df["departed"] = df["departed"].astype(int)
+    df["entered"] = df["entered"].astype(int)
+    df["arrived"] = df["arrived"].astype(int)
+    df["left"] = df["left"].astype(int)
 
-    df['inflow'] = (df['departed'] + df['entered']) * 12
-    df['outflow'] = (df['arrived'] + df['left']) * 12
+    df["inflow"] = (df["departed"] + df["entered"]) * 12
+    df["outflow"] = (df["arrived"] + df["left"]) * 12
 
-    df = df[['begin', 'id', 'laneDensity', 'inflow', 'outflow']]
+    df = df[["begin", "id", "laneDensity", "inflow", "outflow"]]
 
     # Rename "begin" column to "time" and id column to "lane_ids"
-    df = df.rename(columns={'begin': 'time', 'id': 'lane_ids'})
+    df = df.rename(columns={"begin": "time", "id": "lane_ids"})
     df = df.dropna()
 
     time_list, flowdensity_time = get_data_group(df)
-    Qn, Kn = get_MFD_property(time_list, flowdensity_time,total_lane_length,lane_length_dict)
+    Qn, Kn = get_MFD_property(
+        time_list, flowdensity_time, total_lane_length, lane_length_dict
+    )
 
-    # Qpeak itu nilai maks sumbu y dari MFD tapi dari sumbu y itu yang dipake sumbu x nya, Q gridlock itu nilai maks sumbu x dari MFD, 
-    Qpeak = np.max(Qn) #di dalem kurung itu nilai maksimum dari mfd
+    # Qpeak itu nilai maks sumbu y dari MFD tapi dari sumbu y itu yang dipake sumbu x nya, Q gridlock itu nilai maks sumbu x dari MFD,
+    Qpeak = np.max(Qn)  # di dalem kurung itu nilai maksimum dari mfd
     Kpeak = Kn[np.argmax(Qpeak)]
-    Qgridlock = np.max(Kn) # Nilai ini diambil dari titik paling kanan di mfd
+    Qgridlock = np.max(Kn)  # Nilai ini diambil dari titik paling kanan di mfd
     Kgridlock = Qn[np.argmax(Qgridlock)]
 
     return Qpeak, Kpeak, Qgridlock, Kgridlock
-
--- PSO.py
+++ PSO.py
@@ -3,39 +3,43 @@ import matplotlib.pyplot as plt
 from Simulasi import simulasi
 from Objective import objective_function
 from MFD import MFD
+from csv import writer
 
 time_start = 8
 time_end = 8.01
 runs = 1
 
+
 max_iteration = 3
 error_max = 0.01
-#Nilai c1,c2, dan W divariasikan hingga menghasilkan hasil yang paling optimal
-c1 = 0.1 # cognitif parameter
-c2 = 0.1 # social coefficient
-W = 0.8 # weight
+# Nilai c1,c2, dan W divariasikan hingga menghasilkan hasil yang paling optimal
+c1 = 0.1  # cognitif parameter
+c2 = 0.1  # social coefficient
+W = 0.8  # weight
 jumlah_partikel = 10
 
 # random x1 x2 sesuai jumlah partikel
-x1 = 5*np.random.rand(1,jumlah_partikel)[0]
-x2 = 5*np.random.rand(1,jumlah_partikel)[0]
+x1 = 5 * np.random.rand(1, jumlah_partikel)[0]
+x2 = 5 * np.random.rand(1, jumlah_partikel)[0]
 
 # random vx1 vx2 sesuai jumlah partikel
-vx1 = np.random.rand(1,jumlah_partikel)[0]
-vx2 = np.random.rand(1,jumlah_partikel)[0]
+vx1 = np.random.rand(1, jumlah_partikel)[0]
+vx2 = np.random.rand(1, jumlah_partikel)[0]
 
-#Cari Objective Function
+# Cari Objective Function
 objective = [0 for i in range(jumlah_partikel)]
 index_terbaik = np.argmax(objective)
 
-pbest = np.array([x1,x2])
-gbest = np.array([[pbest[0,index_terbaik]],[pbest[1,index_terbaik]]]) # ambil nilai komponen kolom ke index terbaik dan seluruh baris
+pbest = np.array([x1, x2])
+gbest = np.array(
+    [[pbest[0, index_terbaik]], [pbest[1, index_terbaik]]]
+)  # ambil nilai komponen kolom ke index terbaik dan seluruh baris
 # print(index_terbaik)
 # print(pbest)
 # print(gbest)
 
-v = np.array([vx1,vx2])
-x = np.array([x1,x2])
+v = np.array([vx1, vx2])
+x = np.array([x1, x2])
 r1 = np.random.rand()
 r2 = np.random.rand()
 
@@ -43,44 +47,74 @@ objective_next = [0 for i in range(jumlah_partikel)]
 
 iter = 0
 while True:
-	v_next = W * v + c1*r1*(pbest - x) + c2*r2*(gbest - x)
-	x_next = x + v_next
-	x1_next = x_next[0,:]
-	x2_next = x_next[1,:]
-	for i in range(jumlah_partikel):
-		# objective_next[i] = validasi(beta=0.5, density=0.1, time_start=x1_next[i], time_end=x2_next[i])
-		simulasi(beta=x1_next[i],density=x2_next[i], time_start=time_start, time_end=time_end) #nanti x1 x2 diganti beta sama density
-		Qpeak, Kpeak, Qgridlock, Kgridlock = MFD()
-		objective_next[i] = objective_function(Qpeak, Kpeak, Qgridlock, Kgridlock)
+    v_next = W * v + c1 * r1 * (pbest - x) + c2 * r2 * (gbest - x)
+    x_next = x + v_next
+    x1_next = x_next[0, :]
+    x2_next = x_next[1, :]
+    for i in range(jumlah_partikel):
+        # objective_next[i] = validasi(beta=0.5, density=0.1, time_start=x1_next[i], time_end=x2_next[i])
+        simulasi(
+            beta=x1_next[i],
+            density=x2_next[i],
+            time_start=time_start,
+            time_end=time_end,
+        )  # nanti x1 x2 diganti beta sama density
+        Qpeak, Kpeak, Qgridlock, Kgridlock = MFD()
+        objective_next[i] = objective_function(Qpeak, Kpeak, Qgridlock, Kgridlock)
+        data_list = [
+            iter,
+            i,
+            Qpeak,
+            Kpeak,
+            Qgridlock,
+            Kgridlock,
+            objective_next[i],
+            x1_next[i],
+            x2_next[i],
+        ]
+        with open("data.csv", "a") as f_object:
+            # Pass this file object to csv.writer()
+            # and get a writer object
+            writer_object = writer(f_object)
 
-	#objective_next = (x1_next-3.14)**2 + (x2_next-2.72)**2 + np.sin(3*x1_next+1.41) + np.sin(4*x2_next-1.73)
-	
-	for j in range(jumlah_partikel):
-		if objective_next[j] > objective[j]:
-			pbest[0,j] = x1_next[j]
-			pbest[1,j] = x2_next[j]
-		else:
-			None
-			
-	objective = objective_next
-	index_terbaik = np.argmax(objective_next)
-	gbest = np.array([[pbest[0,index_terbaik]],[pbest[1,index_terbaik]]]) # ambil nilai komponen kolom ke index terbaik dan seluruh baris
+            # Pass the list as an argument into
+            # the writerow()
+            writer_object.writerow(data_list)
 
-	v = v_next
-	x = np.array([x1_next,x2_next])
+            # Close the file object
+            f_object.close()
 
-	error = pbest-gbest
-	status = np.linalg.norm(error,axis=0) < error_max
-	
-	if status.all() or iter > max_iteration:
-		break
-	
-	iter = iter + 1
+    # objective_next = (x1_next-3.14)**2 + (x2_next-2.72)**2 + np.sin(3*x1_next+1.41) + np.sin(4*x2_next-1.73)
+
+    for j in range(jumlah_partikel):
+        if objective_next[j] > objective[j]:
+            pbest[0, j] = x1_next[j]
+            pbest[1, j] = x2_next[j]
+        else:
+            None
+
+    objective = objective_next
+    index_terbaik = np.argmax(objective_next)
+    gbest = np.array(
+        [[pbest[0, index_terbaik]], [pbest[1, index_terbaik]]]
+    )  # ambil nilai komponen kolom ke index terbaik dan seluruh baris
+
+    v = v_next
+    x = np.array([x1_next, x2_next])
+
+    error = pbest - gbest
+    status = np.linalg.norm(error, axis=0) < error_max
+
+    if status.all() or iter > max_iteration:
+        break
+
+    iter = iter + 1
+
 
 # def f(x,y):
 #     "Objective function"
 #     return J
-    
+
 # # Compute and plot the function in 3D within [0,5]x[0,5]
 # x_mesh, y_mesh = np.array(np.meshgrid(np.linspace(0,5,100), np.linspace(0,5,100)))
 # z_mesh = f(x_mesh, y_mesh)
@@ -108,5 +142,3 @@ while True:
 # print(objective_next)
 # print(gbest)
 # print(iter)
-
-    
